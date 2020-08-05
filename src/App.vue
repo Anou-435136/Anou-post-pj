3b6b80234e45f0ae22a827778fc76452e007139d
Anou <aanou1129@icloud.com>
2020-08-05 15:16:17 +0900

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <input type="text" v-model="postData" />
+    <button @click="getAddress">住所自動表示</button>
+    <p>Address:{{address}}</p>
   </div>
 </template>
 
+<script>
+import {mapState} from "vuex";
+export default {
+  data() {
+    return {
+      postData: ""
+    };
+  },
+  computed: {
+    ...mapState({
+      address() {
+      return this.$store.state.postData.address;
+    }})
+  },
+  methods: {
+    getAddress() {
+      this.$store.commit("getAddress", this.postData);
+      this.$store.dispatch("getAddressAction");
+    }
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,28 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    postData: "",
+    address: ""
   },
   mutations: {
+    getAddress(state, postData, address) {
+      state.postData = postData;
+      state.address = address;
+      
+    }
   },
   actions: {
-  },
-  modules: {
+    async getAddressAction(context) {
+      let url = await axios.get("https://apis.postcode-jp.com/api/v3/postcodes/" + this.zipcode + "?apiKey=PJDyOjziRdDC2TwwVUhYrGdm6o88j2VewRIxLZo" + this.apiKey)
+        .then(response => {
+          url.adress = response.data.fullAddress;
+        });
+      context.commit("getAddress", url);
+    }
   }
-})
+});
