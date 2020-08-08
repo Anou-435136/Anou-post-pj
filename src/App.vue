b33f037e92f6c5201518627998b2a4f9b65d8888
Anou <aanou1129@icloud.com>
2020-08-08 10:29:26 +0900

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,21 @@
   <div id="app">
     <input type="text" v-model="postData" />
     <button @click="getAddress">住所自動表示</button>
-    <p>Address:{{address}}</p>
+    <p>Address:{{$store.state.address}}</p>
   </div>
 </template>
 
 <script>
-import {mapState} from "vuex";
 export default {
   data() {
     return {
-      postData: ""
+      postData: "",
     };
-  },
-  computed: {
-    ...mapState({
-      address() {
-      return this.$store.state.postData.address;
-    }})
   },
   methods: {
     getAddress() {
-      this.$store.commit("getAddress", this.postData);
-      this.$store.dispatch("getAddressAction");
-    }
-  }
+      this.$store.dispatch("getAddressAction", this.postData);
+    },
+  },
 };
 </script>
-
-<style>
-
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,23 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import axios from 'axios';
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    postData: "",
-    address: ""
+    address: "",
   },
   mutations: {
-    getAddress(state, postData, address) {
-      state.postData = postData;
-      state.address = address;
-      
-    }
+    getAddress(state, url) {
+      state.address = url.data.allAddress;
+    },
   },
   actions: {
-    async getAddressAction(context) {
-      let url = await axios.get("https://apis.postcode-jp.com/api/v3/postcodes/" + this.zipcode + "?apiKey=PJDyOjziRdDC2TwwVUhYrGdm6o88j2VewRIxLZo" + this.apiKey)
-        .then(response => {
-          url.adress = response.data.fullAddress;
-        });
-      context.commit("getAddress", url);
-    }
-  }
+    async getAddressAction(context, postData) {
+      let data = await axios.get(
+        "https://apis.postcode-jp.com/api/v3/postcodes/" + postData + "?apiKey=PJDyOjziRdDC2TwwVUhYrGdm6o88j2VewRIxLZo");
+      context.commit("getAddress", data);
+    },
+  },
 });
